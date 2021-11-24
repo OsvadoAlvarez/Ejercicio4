e7e5b289074a64110c9ab6b9bb5293b6042fa313
ozzy <osviac13@hotmail.com>
2021-11-24 08:31:10 -0700

    ejercicio en sitio completo

---
+++ Web_server/readme.txt
@@ -0,0 +1,5 @@
+This template / effect / code has been created by Clement Girault.
+You can customize and check it out on its original site on the following link:
+https://codepen.io/clementGir/pen/PqGyMq
+
+Thank you
---
+++ Web_server/server.js
@@ -0,0 +1,20 @@
+const http = require ('http');
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+
+    res.setHeader('Context-TypeError', 'text/html');
+
+    res.end('<h1>Osvaldo Camacho</h1>');
+
+});
+
+server.listen(port,hostname, () =>{
+    console.log(`el servidor se esta ejecutado
+    en http: //${hostname}:3000/`);
+});
